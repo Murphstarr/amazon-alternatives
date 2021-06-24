52f2c00218f7cbb20b1d19c422f372730b517800
Daniel Murphy <daniel.murphyx@gmail.com>
2021-06-23 22:23:44 -0700

    started working on user input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Under Construction
-        </p>
-      </header>
+  const [submitting, setSubmitting] = useState(false);
+  const handleSubmit = event => {
+    event.preventDefault();
+   setSubmitting(true);
+
+   setTimeout(() => {
+     setSubmitting(false);
+   }, 3000)
+ }
+
+  return(
+    <div className="wrapper">
+      <h1>Enter a URL</h1>
+      {submitting &&
+       <div>Loading...</div>
+     }
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Link to Product</p>
+            <input name="name" />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
